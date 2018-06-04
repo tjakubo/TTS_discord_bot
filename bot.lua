d77f5b7d1a4794287df0cb87fd74237c2d65605a
tjakubo2 <tofix112@gmail.com>
2018-06-04 09:34:26 +0200

    Pull with reporting

--- bot.lua
+++ bot.lua
@@ -6,20 +6,28 @@ client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
+
 client:on('messageCreate', function(message)
-	if message.content == '!ping' then
-		message.channel:send('Pong!')
-	end
+    if message.content == '!ping' then
+        message.channel:send('Pong!')
+    end
 
-	if message.content == '!quit' then
-		message.channel:send('Exiting....')
-		os.exit()
-	end
+    if message.content == '!quit' then
+        message.channel:send('Exiting....')
+        os.exit()
+    end
 
-	if message.content == '!update' then
-		os.execute('git pull --ff-only')
-		os.exit()
-	end
+    if message.content == '!update' then
+        message.channel:send('Updating...')
+        local p, err = io.popen('git pull --ff-only')
+        if not p then
+            message.channel:send('Error executing pull: ``' .. err .. '``')
+            return
+        end
+        message.channel:send('```\n' .. p:read('*all') .. '\n```')
+        message.channel:send('Restarting....')
+        os.exit()
+    end
     
     if message.content == '!woo' then
         message.channel:send('Wee')
