0acfa6abcc3aacd6da1c2edc02f116327907c010
tjakubo2 <tofix112@gmail.com>
2018-06-04 09:50:43 +0200

    Sandbox exec prototype

--- bot.lua
+++ bot.lua
@@ -6,6 +6,30 @@ client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
+do
+    local sandboxEnv = {}
+    
+    function sandbox(script)
+        local output = {}
+        sandboxEnv.print = function(...)
+            local o = {...}
+            for k in pairs(o) do o[k] = tostring(o[k]) end
+            output[#output+1] = table.concat(o, '\t')
+        end
+        
+        local fcn, err = loadstring(script)
+        if not fcn then
+            return false, 'Load error:\n' .. err
+        end
+        
+        setfenv(fcn, sandboxEnv)
+        local res, err = pcall(fcn)
+        if not res then
+            return false, 'Runtime error:\n' .. err
+        end
+        return true, table.concat(output, '\n')
+    end
+end
 
 client:on('messageCreate', function(message)
     if message.content == '!ping' then
@@ -31,6 +55,16 @@ client:on('messageCreate', function(message)
     
     if message.content == '!woo' then
         message.channel:send('Wee')
+    end
+    
+    if message.content:find('^!exec') then
+        local script = message.content:match('```lua(.-)```') or message.content:match('```(.-)```')
+        if not script then
+            message.channel:send('Code not found in the !exec message. Make sure to wrap it in three backtics (`).')
+            return
+        end
+        local res, out = sandbox(script)
+        message.channel:send(out)
     end
 
 end)
