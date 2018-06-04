29703b60fcc0b30703835f461f702201b503ccd1
tjakubo2 <tofix112@gmail.com>
2018-06-04 15:48:52 +0200

    Add Discordia install to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Install luvit as per https://luvit.io/install.html
 curl -L https://github.com/luvit/lit/raw/master/get-lit.sh | sh
+# Install Discordia
+./lit install SinisterRectus/discordia
